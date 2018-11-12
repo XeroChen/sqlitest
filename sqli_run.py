fa9052c28d9b326cabc87e26a5864df820daf567
Xero.Chen <kissingger@gmail.com>
2018-11-13 03:22:03 +0800

    sample

--- sqli_run.py
+++ sqli_run.py
@@ -22,39 +22,47 @@ def all_files(root, patterns='*', single_level=False, yield_folders=False):
             break
 
 
-class SQLiSample(object):
+class CSample(object):
     def __init__(self):
-        self.loaded = ""
-        self.sample_string = []
+        # self.loaded = []
+        self.sample = dict()
 
     def load_from_file(self, filename):
         sf = open(filename, 'r')
-        line_num = 1
+        line_num = 0
         for line in sf:
-
-            if line.strip().startswith("#"):
-                continue
-            self.sample_string.append((filename, line_num, line))
             line_num += 1
+            line = line.partition("#")[0].partition("--")[0].strip()
+
+            if line == "" or line.startswith("#") or line.startswith("--"):
+                continue
+
+            if filename not in self.sample:
+                self.sample[filename] = [(line_num, line)]
+            else:
+                self.sample[filename].append((line_num, line))
 
     def load_from_dir(self, dirname, filename_pattern):
         for test_file in all_files(dirname, filename_pattern):
             self.load_from_file(test_file)
 
-    def sample_iter(self):
-        for smpl in self.sample_string:
-            yield smpl
+    # def sample_iter(self):
+    #     for smpl_fn, smpl_ln in self.sample:
+    #         yield smpl_fn, smpl_ln[0], smpl_ln[1]
 
 
-class CSampleFixture(object):
+class CHttpRunner(object):
     def __init__(self):
-        pass
+        self.url_param = ""
 
     def run(self):
         pass
 
-    def run_sample(self, sample_obj):
-        # requests.get()
+    def set_url_param(self):
+        pass
+
+    def url_test(self, url_sample, payload_sample):
+        pass
 
     def header_test(self):
         pass
@@ -63,7 +71,8 @@ class CSampleFixture(object):
         pass
 
 
-def run_test():
-    uri = "http://192.168.25.10"
-    method = "GET"
-    requests.Request()
+if __name__ == "__main__":
+    sqli_samples = CSample()
+    sqli_samples.load_from_dir("D:\code\github.com\libinjection\data", "sqli*")
+    for smpl_fn,  smpl_lns in sqli_samples.sample.iteritems():
+        print "%d samples in %s loaded." % (len(smpl_lns), smpl_fn)
