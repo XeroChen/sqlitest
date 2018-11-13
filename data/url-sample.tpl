f0357440797ea1bd475d628344fb8aabae4980a9
Xero.Chen <kyller_clemens@163.com>
2018-11-13 18:38:44 +0800

    test case init

---
+++ data/url-sample.tpl
@@ -0,0 +1,1 @@
+http://192.168.25.10/testparam?a=1{{.payload}}
--- sqli_run.py
+++ sqli_run.py
@@ -1,9 +1,24 @@
 # -*- coding: UTF-8 -*-
 
 import requests
-
-import os
 import fnmatch
+import logging
+import yaml
+import logging.config
+import os
+
+log = logging.getLogger("main_logger")
+
+
+def setup_logging(default_path='config.yaml', default_level=logging.INFO):
+    path = default_path
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            config = yaml.load(f)
+            logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
 
 
 def all_files(root, patterns='*', single_level=False, yield_folders=False):
@@ -41,28 +56,41 @@ class CSample(object):
                 self.sample[filename] = [(line_num, line)]
             else:
                 self.sample[filename].append((line_num, line))
+        print "%-8d samples in %s" % (len(self.sample[filename]), filename)
 
     def load_from_dir(self, dirname, filename_pattern):
         for test_file in all_files(dirname, filename_pattern):
             self.load_from_file(test_file)
 
-    # def sample_iter(self):
-    #     for smpl_fn, smpl_ln in self.sample:
-    #         yield smpl_fn, smpl_ln[0], smpl_ln[1]
+    def get_loaded_samples(self):
+        return self.sample
+
+    def sample_iter(self):
+        for smpl_fn, smpl_lns in self.sample.iteritems():
+            for smpl_ln in smpl_lns:
+                yield smpl_fn, smpl_ln[0], smpl_ln[1]
 
 
-class CHttpRunner(object):
+class CSampleRunner(object):
     def __init__(self):
-        self.url_param = ""
+        self.url_sample = None
+        self.url_payload = None
 
     def run(self):
         pass
 
-    def set_url_param(self):
-        pass
+    def set_url_sample(self, url_sample):
+        self.url_sample = url_sample
 
-    def url_test(self, url_sample, payload_sample):
-        pass
+    def set_url_payload(self, url_payload):
+        self.url_payload = url_payload
+
+    def generate_url(self):
+        if not self.url_payload:
+            print
+        for templ in self.url_sample.sample_iter():
+            for pld in self.url_payload.sample_iter():
+                yield templ[2].replace("{{.payload}}", pld[2])
 
     def header_test(self):
         pass
@@ -72,7 +100,22 @@ class CHttpRunner(object):
 
 
 if __name__ == "__main__":
+    setup_logging("./log_conf.yaml")
     sqli_samples = CSample()
-    sqli_samples.load_from_dir("D:\code\github.com\libinjection\data", "sqli*")
-    for smpl_fn,  smpl_lns in sqli_samples.sample.iteritems():
-        print "%d samples in %s loaded." % (len(smpl_lns), smpl_fn)
+    sqli_samples.load_from_dir("./data", "sqli*")
+
+    url_tpl = CSample()
+    url_tpl.load_from_file("./data/url-sample.tpl")
+
+    runner = CSampleRunner()
+    runner.set_url_sample(url_tpl)
+    runner.set_url_payload(sqli_samples)
+
+    for test_url in runner.generate_url():
+        try:
+            r = requests.get(test_url)
+            if r.status_code != 200:
+                print "\n%s\nstatus code: %d\n" % (test_url, r.status_code)
+        except requests.exceptions.ConnectionError, e:
+            print "\n%s\nerror: %s\n" % (test_url, e)
+
